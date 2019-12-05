78a1c79a52103e89600354b0ade9866e15ff8163
Mattäus S. Isenberg <mattaus@gmx.com>
2019-12-05 13:08:19 -0800

    functional switching

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 let arrayOfHorns = [];
+let firstSet = [];
+let secondSet = [];
+let dataSetActive = false;
+let dsOneRenderStatus = false;
+let dsTwoRenderStatus = false;
+
+
 
 function populateList(){
     arrayOfHorns.forEach(function(type){
@@ -47,29 +54,85 @@ function HornedAnimal(title, img, description, keyword, horns){
     clone.find('p').text(this.description);
     clone.removeAttr('id');
     console.log(clone);
-  
+    $('horned').empty();
     $('#horned').append(clone);
+    dsOneRenderStatus = true;
+  };
+
+  HornedAnimal.prototype.renderWithJqueryClone2 = function(){
+    let clone = $('#photo-template2').clone();
+  
+    clone.find('h2').text(this.title);
+    clone.find('img').attr('src', this.img);
+    clone.find('img').attr('alt', this.keyword);
+    clone.find('p').text(this.description);
+    clone.removeAttr('id');
+    console.log(clone);
+    $('#horned2').hide();
+    $('#horned2').append(clone);
   };
 
 
+
 $.get('page-1.json').then(
-    (data) => {
-      console.log(data);
-      data.forEach(hornedObjFromFile => {
-        let hornedAnimal = new HornedAnimal(hornedObjFromFile.title, hornedObjFromFile.image_url, hornedObjFromFile.description, hornedObjFromFile.keyword, hornedObjFromFile.horns);
-        hornedAnimal.renderWithJqueryClone();
-        selectFiltering(hornedObjFromFile.keyword);
-        
-      });
-      populateList();
+  (data) => {
+    console.log(data);
+    data.forEach(hornedObjFromFile => {
+      let hornedAnimal = new HornedAnimal(hornedObjFromFile.title, hornedObjFromFile.image_url, hornedObjFromFile.description, hornedObjFromFile.keyword, hornedObjFromFile.horns);
+      hornedAnimal.renderWithJqueryClone();
+      selectFiltering(hornedObjFromFile.keyword);
+      dataSetActive = true;
+      firstSet.push(hornedAnimal);
+      
     });
+    populateList();
+  });
+
+$.get('page-2.json').then(
+  (data) => {
+    console.log(data);
+    data.forEach(hornedObjFromFile => {
+      let hornedAnimal = new HornedAnimal(hornedObjFromFile.title, hornedObjFromFile.image_url, hornedObjFromFile.description, hornedObjFromFile.keyword, hornedObjFromFile.horns);
+      hornedAnimal.renderWithJqueryClone2();
+      selectFiltering(hornedObjFromFile.keyword);
+      secondSet.push(hornedAnimal);
+  
+    });
+    populateList();
+  });
+
 
 $('select[name="horn"]').on('change', function() {
-    let selection = $(this).val();
-    $('img').hide();
-    $('h2').hide();
-    $('p').hide();
-    $(`img[alt="${selection}"]`).show();
+  const keywordValue = $(this).val();
+  $('div').hide();
+
+  $('img').each(function (currentValue, index, array) {
+    if ($(this).attr('alt') === keywordValue) {
+      $(this).parent().show();
+    }
+  });
 
 });
-      
+    
+function dataSwitch() {
+
+  
+  if (dataSetActive === true){
+    $('#horned').hide();
+    $('#horned2').show();
+
+   dataSetActive = false;
+
+  
+  }
+
+  else if (dataSetActive === false){
+    $('#horned2').hide();
+       $('#horned').show();
+
+   dataSetActive = true;
+  }
+}
+
+
+
