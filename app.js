a56b91b111459f755f211c84659b1fd3722ddec0
jjdeforrest <jjdeforrest@users.noreply.github.com>
2019-12-05 13:29:04 -0800

    handlebars

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 let arrayOfHorns = [];
 
-function populateList(){
-    arrayOfHorns.forEach(function(type){
-        let selectDD = $('#horn');
-        let option = document.createElement('option');
-        option.value = type;
-        option.text = type;
-        selectDD.append(option);
-    });
+function populateList() {
+  arrayOfHorns.forEach(function (type) {
+    let selectDD = $('#horn');
+    let option = document.createElement('option');
+    option.value = type;
+    option.text = type;
+    selectDD.append(option);
+  });
 }
 
-function selectFiltering(keyword){
-    if(!arrayOfHorns.includes(keyword)){
-        arrayOfHorns.push(keyword);
-    }
-    console.log(`Array is ${arrayOfHorns}`);
-}
-
-
-
-function HornedAnimal(title, img, description, keyword, horns){
-    this.title = title;
-    this.img = img;
-    this.description = description;
-    this.keyword = keyword;
-    this.horns = horns;
+function selectFiltering(keyword) {
+  if (!arrayOfHorns.includes(keyword)) {
+    arrayOfHorns.push(keyword);
   }
-  
-  HornedAnimal.prototype.renderWithJquery = function(){
-    $('#photo-template').append(`
+  console.log(`Array is ${arrayOfHorns}`);
+}
+
+
+
+function HornedAnimal(title, img, description, keyword, horns) {
+  this.title = title;
+  this.img = img;
+  this.description = description;
+  this.keyword = keyword;
+  this.horns = horns;
+}
+
+HornedAnimal.prototype.renderWithJquery = function () {
+  $('#photo-template').append(`
       <div class = "first">
         <h2>${this.title}</h2>
         <img src="${this.img}"></img>
@@ -36,40 +36,49 @@ function HornedAnimal(title, img, description, keyword, horns){
         <p>Horns: ${this.horns}</p>
       </div>
     `);
-  };
-  
-  HornedAnimal.prototype.renderWithJqueryClone = function(){
-    let clone = $('#photo-template').clone();
-  
-    clone.find('h2').text(this.title);
-    clone.find('img').attr('src', this.img);
-    clone.find('img').attr('alt', this.keyword);
-    clone.find('p').text(this.description);
-    clone.removeAttr('id');
-    console.log(clone);
-  
-    $('#horned').append(clone);
-  };
+};
+
+HornedAnimal.prototype.renderWithJqueryClone = function () {
+  let clone = $('#photo-template').clone();
+  clone.find('h2').text(this.title);
+  clone.find('img').attr('src', this.img);
+  clone.find('img').attr('alt', this.keyword);
+  clone.find('p').text(this.description);
+  clone.removeAttr('id');
+  console.log(clone);
+
+  $('#horned').append(clone);
+};
 
 
 $.get('page-1.json').then(
-    (data) => {
-      console.log(data);
-      data.forEach(hornedObjFromFile => {
-        let hornedAnimal = new HornedAnimal(hornedObjFromFile.title, hornedObjFromFile.image_url, hornedObjFromFile.description, hornedObjFromFile.keyword, hornedObjFromFile.horns);
-        hornedAnimal.renderWithJqueryClone();
-        selectFiltering(hornedObjFromFile.keyword);
-        
-      });
-      populateList();
-    });
+  (data) => {
+    console.log(data);
+    data.forEach(hornedObjFromFile => {
+      let hornedAnimal = new HornedAnimal(hornedObjFromFile.title, hornedObjFromFile.image_url, hornedObjFromFile.description, hornedObjFromFile.keyword, hornedObjFromFile.horns);
+      hornedAnimal.renderWithJqueryClone();
+      selectFiltering(hornedObjFromFile.keyword);
 
-$('select[name="horn"]').on('change', function() {
-    let selection = $(this).val();
-    $('img').hide();
-    $('h2').hide();
-    $('p').hide();
-    $(`img[alt="${selection}"]`).show();
+    });
+    populateList();
+  });
+
+$('select[name="horn"]').on('change', function () {
+  let selection = $(this).val();
+  $('img').hide();
+  $('h2').hide();
+  $('p').hide();
+  $(`img[alt="${selection}"]`).show();
 
 });
-      
+
+var template = $('#photo-template').html();
+var compile = Handlebars.compile(template);
+
+var result = compile(arrayOfHorns);
+$('#horned').append(result);
+
+HornedAnimal.prototype.render = function(){
+  $('#horned').append(template(this));
+};
+
